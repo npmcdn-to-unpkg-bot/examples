3d9f383a2d2784a169e5ce848c30fa7de5eda284
Slavian Petrov <slavianp@mail.bg>
2016-04-18 21:44:13 +0300

    ..

--- grunt/src/main/js/slavi-log.js
+++ grunt/src/main/js/slavi-log.js
@@ -6,43 +6,46 @@ define("slavi-log", ["jquery"], function($) {
 		uiLogEnabled: true,
 		consoleLogEnabled: true,
 	};
+
+	function stringify(obj) {
+		var seen = [];
+		return JSON.stringify(obj, function(key, val) {
+			if (val != null && typeof val === "object") {
+				if (seen.indexOf(val) >= 0) {
+					return "... cyclic ref ...";
+				}
+				seen.push(val);
+			}
+			return val;
+		});
+	};
+
+	function log(message) {
+		var uilog = $(settings.uiLogSelector);
+		if (!((settings.uiLogEnabled && !uilog.empty()) || settings.consoleLogEnabled))
+			return;
+		if (typeof message !== "string")
+			message = stringify(message);
+		if (settings.uiLogEnabled && !uilog.empty()) {
+			$(settings.uiLogTag).text(message).prependTo(uilog);
+			var count = uilog.children().length;
+			while (count-- > settings.uiLogItemsMax) {
+				uilog.children().remove(":last-child");
+			}
+			uilog.scrollTop( 0 );
+		}
+		if (settings.consoleLogEnabled) {
+			console.log(message);
+		}
+	};
+
+	function logClear() {
+		$(settings.uiLogSelector).children().remove();
+	};
 	
 	return {
-		stringify: function(obj) {
-			seen = [];
-			return JSON.stringify(obj, function(key, val) {
-				if (val != null && typeof val === "object") {
-					if (seen.indexOf(val) >= 0) {
-						return "... cyclic ref ...";
-					}
-					seen.push(val);
-				}
-				return val;
-			});
-		},
-		
-		log: function(message) {
-			var uilog = $(settings.uiLogSelector);
-			if (!((settings.uiLogEnabled && !uilog.empty()) || settings.consoleLogEnabled))
-				return;
-			if (typeof message !== "string")
-				message = this.stringify(message);
-			if (settings.uiLogEnabled && !uilog.empty()) {
-				$(settings.uiLogTag).text(message).prependTo(uilog);
-				var count = uilog.children().length;
-				while (count-- > settings.uiLogItemsMax) {
-					uilog.children().remove(":last-child");
-				}
-				uilog.scrollTop( 0 );
-			}
-			if (settings.consoleLogEnabled) {
-				console.log(message);
-			}
-		},
-		
-		logClear: function() {
-			$(settings.uiLogSelector).children().remove();
-		},
-		
+		stringify: stringify,
+		log: log,
+		logClear: logClear,
 	};
 });
