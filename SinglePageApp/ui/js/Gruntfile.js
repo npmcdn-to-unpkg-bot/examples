b2b07ff8470dec5acd91aefa100b389f4d125f4e
Slavian Petrov <slavianp@mail.bg>
2016-06-10 23:52:00 +0300

    .

--- SinglePageApp/ui/js/Gruntfile.js
+++ SinglePageApp/ui/js/Gruntfile.js
@@ -11,43 +11,6 @@ module.exports = function( grunt ) {
 			srcjs: "<%= params.src %>/js",
 			nodemodules: "node_modules/"
 		},
-/*		requirejs: { 
-			compile: {
-				options: {
-					baseUrl: "<%= params.srcjs %>",
-					name: "main",
-					optimize: "none",
-					include: [ "requirejs" ],
-					paths: {
-						"requirejs":			"<%= params.nodemodules %>/requirejs/require",
-						//"moment":				"<%= params.nodemodules %>/moment/moment",
-						"jquery":				"<%= params.nodemodules %>/jquery/dist/jquery",
-						//"datatables.net":		"<%= params.nodemodules %>/datatables.net/js/jquery.dataTables",
-						//"datatables.net-dt":	"<%= params.nodemodules %>/datatables.net-dt",
-
-						//"es6-shim":				"<%= params.nodemodules %>/es6-shim/es6-shim",
-						//"system-polyfills":		"<%= params.nodemodules %>/systemjs/dist/system-polyfills",
-						//"shims_for_IE":			"<%= params.nodemodules %>/angular2/es6/dev/src/testing/shims_for_IE",
-						//"angular2-polyfills":	"<%= params.nodemodules %>/angular2/bundles/angular2-polyfills",
-						//"system.src":			"<%= params.nodemodules %>/systemjs/dist/system.src",
-						//"Rx":					"<%= params.nodemodules %>/rxjs/bundles/Rx.umd",
-						//"angular2":				"<%= params.nodemodules %>/angular2/bundles/angular2-all.umd",
-
-						"angular":				"<%= params.nodemodules %>/angular/angular",
-						//"angular-route":		"<%= params.nodemodules %>/angular-route/angular-route",
-						"angular-route":		"<%= params.nodemodules %>/@angular/router/angular1/angular_1_router",
-						"angular-resource":		"<%= params.nodemodules %>/angular-resource/angular-resource",
-						//"ng-grid":				"<%= params.nodemodules %>/ng-grid/build/ng-grid",
-					},
-					useStrict: true,
-					wrap: {
-						start: "// Built on <%= now %>\n'use strict';\n",
-						end: ""
-					},
-					out: "<%= params.dest %>/alljs.js",
-				}
-			}
-		},*/
 		jshint: {
 			options: {
 				multistr: true,
@@ -118,17 +81,7 @@ module.exports = function( grunt ) {
 					src: ["<%= params.dest %>/app.js"],
 					dest: "<%= params.dest %>/app.min.js",
 				}]
-			},
-/*			all: {
-				files: [{
-					expand: true,
-					cwd: "<%= params.dest %>",
-					src: ["*.js", "!*.min.js"],
-					dest: "<%= params.dest %>/",
-					ext: ".min.js",
-					extDot: "last"
-				}]
-			} */
+			}
 		},
 		newer: {
 			ngtemplates_app: {
@@ -137,20 +90,10 @@ module.exports = function( grunt ) {
 				options: { tasks: ["ngtemplates:app"] }
 			},
 			concat_libs: {
-				options: { tasks: ["concat:libs"] }
+				options: { tasks: ["concat:libs", "uglify:libs"] }
 			},
 			concat_app: {
-				options: { tasks: ["jshint:app", "concat:app"] }
-			},
-			uglify_libs: {
-				src: ["<%= params.dest %>/libs.js"],
-				dest: "<%= params.dest %>/libs.min.js",
-				options: { tasks: ["uglify:libs"] }
-			},
-			uglify_app: {
-				src: ["<%= params.dest %>/app.js"],
-				dest: "<%= params.dest %>/app.min.js",
-				options: { tasks: ["uglify:app"] }
+				options: { tasks: ["jshint:app", "concat:app", "uglify:app"] }
 			}
 		}
 	};
--- SinglePageApp/ui/js/src/main/js/myapp5/uiBootstrap.ctrl.js
+++ SinglePageApp/ui/js/src/main/js/myapp5/uiBootstrap.ctrl.js
@@ -1,8 +1,9 @@
 var module = angular.module('myapp5');
 
-Implementation.$inject = ["$scope", "slavi-logger"];
-function Implementation($scope, logger) {
+Implementation.$inject = ["$scope", "$sce", "$templateCache", "slavi-logger"];
+function Implementation($scope, $sce, $templateCache, logger) {
 	var that = this;
+	$scope.logger = logger;
 	$scope.oneAtATime = true;
 
 	$scope.groups = [{
@@ -12,9 +13,29 @@ function Implementation($scope, logger) {
 		title: 'Dynamic Group Header - 2',
 		content: 'Dynamic Group Body - 2'
 	}];
-	
+	$scope.popoverUrl = "myapp5/uiBootstrap-htmlPopover.html";
+	$scope.status = {
+		isOpen: false,
+		dirtyCount: 0,
+		isDirty: function() {
+			return $scope.status.dirtyCount !== 0;
+		}
+	};
+
+	$scope.$watch("status.isOpen", function() {
+		$scope.status.dirtyCount = 0;
+	});
+
+	$scope.$watchCollection("logger.items", function() {
+		if (!$scope.status.isOpen) {
+			$scope.status.dirtyCount++;
+		}
+	});
+
+	that.messageCount = 0;
 	that.onAddMessageClick = function() {
-		
+		logger.log("Log message ", ++that.messageCount, " ", $scope.status);
+		// logger.log("HTML:", $scope.htmlPopover);
 	};
 }
 
