99e8c23669c70b19ea2b6b24dffeb56593ad767e
spetrov <spetrov@minervanetworks.com>
2016-06-10 16:45:18 +0300

    .

--- SinglePageApp/ui/js/src/main/js/myapp5/link2.ctrl.js
+++ SinglePageApp/ui/js/src/main/js/myapp5/link2.ctrl.js
@@ -15,14 +15,6 @@ function Implementation(service, $scope, logger) {
 }
 
 module.component('link2', {
-	template: 
-		'<h2>This is LINK 2</h2>\
-		<p ng-hide="$service.deferred.isDone()">LOADING ...</p>\
-		<table ng-show="$service.deferred.isDone()">\
-			<tr ng-repeat="item in $service.items" ng-class="{ selected: $ctrl.even(item) }"> \
-				<td>id: {{item.id}}, name: {{item.name}}</td> \
-			</tr> \
-		</table>\
-		<button ng-click="$service.load(4)">Refresh</button>',
+	templateUrl: "myapp5/link2.html",
 	controller: Implementation
 });
