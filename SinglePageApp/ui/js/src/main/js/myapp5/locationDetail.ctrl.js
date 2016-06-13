8e7fb2d36b008f72d1b02adc818f9083e2cbaefc
spetrov <spetrov@minervanetworks.com>
2016-06-13 18:49:01 +0300

    .

--- SinglePageApp/ui/js/src/main/js/myapp5/locationDetail.ctrl.js
+++ SinglePageApp/ui/js/src/main/js/myapp5/locationDetail.ctrl.js
@@ -3,30 +3,33 @@ var module = angular.module('myapp5');
 Implementation.$inject = ["$scope", "locationService", "slavi-logger"];
 function Implementation($scope, service, logger) {
 	var that = this;
-//	$scope.service = service;
-	
-/*
-	$scope.$watch("service.selectedItem", function(newValue) {
+
+	that.$onChanges = function() {
+		logger.log("onChange");
+	};
+
+	$scope.$watch("$ctrl.item", function(newValue) {
 		$scope.item = angular.copy(newValue);
 	});
-	*/
+
 	that.onDone = function() {
 		logger.log("onDone()");
-/*		service.updateSelected($scope.item)
+		service.updateSelected($scope.item)
 		.then(function(d) {
 			logger.log("Saved ", d);
 			service.selectedItem = null;
 		}, function(e) {
 			logger.log("save failed ", e);
-		});*/
+		});
 	};
 }
 
 module.component('locationDetail', {
 	templateUrl: 'myapp5/locationDetail.html',
+//	scope: {},
 	bindings: {
 		item: "="
 //		save: "&"
 	},
-//	controller: Implementation
+	controller: Implementation
 });
