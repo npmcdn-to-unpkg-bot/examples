7269fe8498a4c449b8780a631d31dc2daac604b3
Slavian Petrov <slavianp@mail.bg>
2016-07-06 00:24:55 +0300

    .

---
+++ SinglePageApp/ui/js/src/main/dummy.js
@@ -0,0 +1,59 @@
+a = {
+    "$viewValue": null,
+    "$modelValue": null,
+    "$validators": {},
+    "$asyncValidators": {},
+    "$parsers": [],
+    "$formatters": [null],
+    "$viewChangeListeners": [],
+    "$untouched": true,
+    "$touched": false,
+    "$pristine": true,
+    "$dirty": false,
+    "$valid": true,
+    "$invalid": false,
+    "$error": {},
+    "$$success": {},
+    "$name": "email",
+    "$$parentForm": {
+        "$error": {},
+        "$$success": {},
+        "$name": "form",
+        "$dirty": false,
+        "$pristine": true,
+        "$valid": true,
+        "$invalid": false,
+        "$submitted": false,
+        "$$parentForm": {},
+        "name": {
+            "$viewValue": null,
+            "$modelValue": null,
+            "$validators": {},
+            "$asyncValidators": {},
+            "$parsers": [],
+            "$formatters": [null],
+            "$viewChangeListeners": [],
+            "$untouched": true,
+            "$touched": false,
+            "$pristine": true,
+            "$dirty": false,
+            "$valid": true,
+            "$invalid": false,
+            "$error": {},
+            "$$success": {},
+            "$name": "name",
+            "$$parentForm": "... cyclic ref ...",
+            "$options": {
+                "updateOn": "blur",
+                "debounce": {
+                    "default": 700,
+                    "blur": 0
+                },
+                "updateOnDefault": true
+            }
+        },
+        "email": "... cyclic ref ..."
+    },
+    "$options": "... cyclic ref ...",
+    "$$parserName": "email"
+}
