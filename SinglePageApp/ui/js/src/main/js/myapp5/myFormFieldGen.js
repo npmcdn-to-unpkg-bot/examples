33689f8b6c30bf11e03e1777dfd8c2681e10d3e3
slavian <slavian@slavian>
2016-08-07 23:48:36 +0300

    .

--- SinglePageApp/ui/js/src/main/js/myapp5/myFormFieldGen.js
+++ SinglePageApp/ui/js/src/main/js/myapp5/myFormFieldGen.js
@@ -1,76 +1,46 @@
 var module = angular.module('myapp5');
 
-Factory.$inject = ["slavi-logger", "$timeout"];
-function Factory(logger, $timeout) {
-	
-	Instance.$inject = ["$scope"];
-	function Instance($scope) {
-		var that = this;
-	
-		console.log("my-form-field-gen created");
-		
-		that.$onChanges = function() {
-			logger.log("onChange");
-		};
-	/*
-		$scope.$watch("$ctrl.item", function(newValue) {
-			$scope.item = angular.copy(newValue);
-		});
-	*/
-		that.onCancel = function() {
-			console.log("onCancel");
-		};
-		
-		that.onSave = function() {
-			console.log("onSave");
-		};
-	}
-	
-	getTemplate.$inject = ["$element", "$attrs"];
-	function getTemplate($element, $attrs) {
-		console.log($element);
-		//var meta = $scope.$eval($attrs[formFieldMeta]);
-		console.log($attrs);
-		return "<pre>WTF?</pre>";
-	}
+Implementation.$inject = ["$scope", "slavi-logger", "$timeout"];
+function Implementation($scope, logger, $timeout) {
+	var that = this;
 
-	// link.$inject = ["$scope", "$element", "$attrs"];
-	function link($scope, $element, $attrs, ctrl, $transclude) {
-		// ctrl.template
-		console.log("inside link");
-		ctrl.template = "<pre>WTF?</pre>";
-		var clone = $transclude($scope, function(clone) {
-			console.log("some where inside transclude");
-		});
-	}
+	that.html = "";
 
-	function compile(element, attr) {
-		console.log("inside compile");
-		dd.template = "<p>waiting even more...</p>";
-		return link;
-	}
-	
-	var dd = {
-//		template: "<p>waiting...</p>",
-		transclude: true,
-		restrict: 'E',
-		scope: false,
-		controller: Instance,
-		compile: compile
+	that.$onChanges = function() {
+		logger.log("onChange");
+
+		switch (that.formFieldMeta.type) {
+		case "radio":
+			that.html = "<my-radio data-model='$ctrl.item[$ctrl.formFieldMeta.name]' data-items='$ctrl.formFieldMeta.values'></my-radio>";
+			break;
+		case "combo":
+			that.html = "<my-combo data-model='$ctrl.item[$ctrl.formFieldMeta.name]' data-items='$ctrl.formFieldMeta.values'></my-combo>";
+			break;
+		case "label":
+			that.html = "<p class='form-control-static'>{{$ctrl.item[$ctrl.formFieldMeta.name]}}</p>";
+			break;
+		case "email":
+			that.html = "<input type='email' class='form-control' name='email' ng-model='$ctrl.item[$ctrl.formFieldMeta.name]' />";
+			break;
+		// case "text":
+		default:
+			that.html = "<input type='text' class='form-control' name='name' ng-model='$ctrl.item[$ctrl.formFieldMeta.name]' \
+				ng-minlength='$ctrl.formFieldMeta.minLength' \
+				ng-maxlength='$ctrl.formFieldMeta.maxLength' \
+				ng-required='$ctrl.formFieldMeta.required' \
+				ng-trim='$ctrl.formFieldMeta.trim' \
+				ng-pattern='$ctrl.formFieldMeta.pattern'/>";
+			break;
+		}
 	};
-	return dd;
 }
 
-module.directive("myFormFieldGen", Factory);
-
-/*
 module.component('myFormFieldGen', {
-	template: getTemplate,
+	template: "<span ng-bind-template='$ctrl.html' ></span>",
 	bindings: {
-//		item: "=",
+		item: "=",
 		formFieldMeta: "="
 //		save: "&"
 	},
 	controller: Implementation
 });
-*/
