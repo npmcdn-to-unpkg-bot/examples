b5f4f19409f7f33af7af3f461f01eed5253cd1a8
spetrov <spetrov@minervanetworks.com>
2016-08-30 17:27:58 +0300

    .

--- SinglePageApp/backend/src/test/java/examples/spa/backend/test/Dummy.java
+++ SinglePageApp/backend/src/test/java/examples/spa/backend/test/Dummy.java
@@ -4,15 +4,38 @@ import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.lang.reflect.TypeVariable;
-import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.cfg.DeserializerFactoryConfig;
-import com.fasterxml.jackson.databind.deser.BeanDeserializerFactory;
-import com.fasterxml.jackson.databind.deser.DefaultDeserializationContext;
-import com.fasterxml.jackson.databind.deser.DeserializerFactory;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+
+import com.fasterxml.jackson.core.Base64Variants;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.BeanDescription;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationConfig;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.cfg.BaseSettings;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.BasicClassIntrospector;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.introspect.SimpleMixInResolver;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.fasterxml.jackson.databind.jsontype.impl.StdSubtypeResolver;
+import com.fasterxml.jackson.databind.ser.BeanPropertyWriter;
+import com.fasterxml.jackson.databind.ser.BeanSerializerFactory;
+import com.fasterxml.jackson.databind.ser.DefaultSerializerProvider;
+import com.fasterxml.jackson.databind.ser.PropertyWriter;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.util.RootNameLookup;
+import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
 
 import examples.spa.backend.component.EntityWithIdControllerBase;
 import examples.spa.backend.component.EntityWithIdJpa;
@@ -20,9 +43,37 @@ import examples.spa.backend.model.EntityWithId;
 
 public class Dummy {
 
+	void configureMapper(ObjectMapper m) {
+		AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
+		AnnotationIntrospector secondary = new JaxbAnnotationIntrospector();
+		AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
+
+		m.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+		//m.setDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX"));
+		m.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		m.setAnnotationIntrospector(pair);
+		m.enable(SerializationFeature.INDENT_OUTPUT);
+	}
+
+	ObjectMapper xmlMapper() {
+		ObjectMapper m = new XmlMapper();
+		configureMapper(m);
+		return m;
+	}
+	
+	ObjectMapper jsonMapper() {
+		ObjectMapper m = new ObjectMapper();
+		configureMapper(m);
+		return m;
+	}
+
+	@XmlAccessorType(XmlAccessType.FIELD)
 	public static class TestEntityWithIdBase<QQ, TT extends Serializable> implements EntityWithId<TT> {
 		QQ qq;
 		
+		@XmlElement
+		String asd;
+
 		public TT getId() {
 			return null;
 		}
@@ -95,18 +146,54 @@ public class Dummy {
 	}
 	
 	void doIt() throws Exception {
+		TestEntityWithIdInt val = new TestEntityWithIdInt();
+		val.asd = "ASD";
+		val.qq = 123;
+		val.setId(222);
+		
+		ObjectMapper m = jsonMapper();
+		System.out.println(m.writeValueAsString(val));
+		System.out.println("----------");
+		
 		Class entType = findEntytyClass(TestControllerInt.class);
 		System.out.println(entType);
+
+		//JsonFactory _jsonFactory = new MappingJsonFactory();
+		//JsonGenerator g = _jsonFactory.createGenerator(NullOutputStream.NULL_OUTPUT_STREAM);
+		BaseSettings DEFAULT_BASE = new BaseSettings(
+				null, // can not share global ClassIntrospector any more (2.5+)
+				new JacksonAnnotationIntrospector(),
+				VisibilityChecker.Std.defaultInstance(), null, TypeFactory.defaultInstance(),
+				null, StdDateFormat.instance, null,
+				Locale.getDefault(),
+				null, // to indicate "use default TimeZone"
+				Base64Variants.getDefaultVariant() // 2.1
+		);
+		BaseSettings base = DEFAULT_BASE.withClassIntrospector(new BasicClassIntrospector());
+		SerializationConfig cfg = new SerializationConfig(base,
+				new StdSubtypeResolver(), new SimpleMixInResolver(null), new RootNameLookup());
+		//cfg.initialize(g);
+		DefaultSerializerProvider _serializerProvider = new DefaultSerializerProvider.Impl();
+		_serializerProvider = _serializerProvider.createInstance(cfg, BeanSerializerFactory.instance);
+		JsonSerializer ser = _serializerProvider.findTypedValueSerializer(entType, true, null);
 		
-		DeserializerFactoryConfig dfc = new DeserializerFactoryConfig();
-		DeserializerFactory df = new BeanDeserializerFactory(dfc);
-		DefaultDeserializationContext.Impl ddc = new DefaultDeserializationContext.Impl(df);
-		TypeFactory tf = TypeFactory.defaultInstance();
-		JavaType jt = tf.constructType(entType);
-		JsonDeserializer jd = ddc.findRootValueDeserializer(jt);
-		System.out.println("-----");
-		System.out.println(jd.getKnownPropertyNames());
-		System.out.println("-----");
+		//Object o = BeanSerializerFactory.instance.createTypeSerializer(cfg, cfg.constructType(entType));
+		//ser = (JsonSerializer) o;
+		
+		BeanDescription bd = cfg.introspect(cfg.constructType(entType));
+		List<BeanPropertyDefinition> props = bd.findProperties();
+		for (BeanPropertyDefinition i : props) {
+			System.out.println(i);
+		}
+		System.out.println("----------");
+		
+		for (Iterator<PropertyWriter> i = ser.properties(); i.hasNext();) {
+			PropertyWriter pw = i.next();
+			if (pw instanceof BeanPropertyWriter) {
+				BeanPropertyWriter bpw = (BeanPropertyWriter) pw;
+				System.out.println(bpw.getName() + ":" + bpw.getType() + ":" + bpw.getGenericPropertyType());
+			}
+		}
 	}
 
 	public static void main(String[] args) throws Exception {
